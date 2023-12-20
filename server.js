355d67ec03e62f2cfa66387d77f0f0160b51c67d
Dhiya <adiadiadha@google.com>
2023-12-20 11:14:26 +0700

    update validation input #85

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { body, validationResult } from 'express-validator';
 
 // Routes
 import jobRouter from "./routes/jobRouter.js";
@@ -18,7 +19,7 @@ import mongoose from "mongoose";
 
 // Middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-
+import e from 'express';
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -31,14 +32,28 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "Post request received!", data: req.body });
+app.post("/api/v1/test", [
+  body('name')
+    .notEmpty()
+    .withMessage('Name is required')
+    .isLength({ min: 50 })
+    .withMessage('Name must be at least 50 characters long')],
+  (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map((error) =>
+      error.msg);
+      return res.status(400).json({ errors: errorMessages })
+  }
+  next();
+},
+  (req, res) => {
+  const { name } = req.body;
+  res.json({ msg: `hello ${name}` });
 });
 
 // Router functions
 app.use("/api/v1/jobs", jobRouter);
-
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: "Not Found" });
